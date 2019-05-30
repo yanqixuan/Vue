c7c93848301e18d19773f4242eeabe8b4d6ed8d4
yanqixuan <1402412256@qq.com>
2019-05-30 22:03:09 +0800

    路由跳转

---
+++ element/src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="main-content">
+        <Header></Header>
+    </div>
+</template>
+<script>
+import Header from './common/Header'
+export default {
+    name:'mainContent',
+    data(){
+        return {
+            msg:'hello world'
+        }
+    },
+    components:{
+        Header
+    }
+}
+</script>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+</style>
+
+
