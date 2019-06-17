62e0c1c317912cdc8a51a28e9448d0d1b3e8e10d
yanqixuan <1402412256@qq.com>
2019-06-17 23:23:19 +0800

    左右联动

--- vue-eleme-appA/src/components/goods/goods.vue
+++ vue-eleme-appA/src/components/goods/goods.vue
@@ -4,12 +4,39 @@
       <div class="menu-wrapper" ref="menuWrapper">
         <ul>
           <li v-for="(item,index) in goods" :key="index" class="menu-item"
-           :class="{'current' : currentIndex == index}">
+           :class="{'current' : currentIndex == index}" @click="selectMenu(index,$event)">
             <span class="text border-1px">
               <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>
               {{item.name}}
             </span>
            </li>
+        </ul>
+      </div>
+      <div class="foods-wrapper" ref="foodsWrapper">
+        <ul>
+          <li v-for="(item,index) in goods" :key="index" class="food-list" ref="foodList">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px"
+               @click="selectFood(index,$event)">
+                <div class="icon">
+                  <img :src="food.icon" alt="" width="57" height="57">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                </div>
+               </li>
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
@@ -24,8 +51,22 @@ export default {
   data(){
     return {
       goods:[],
-      classMap:[]
+      classMap:[],
+      listHeight:[],
+      scrollY:0
     }
+  },
+  computed:{
+     currentIndex(){
+       for (let i =0;i< this.listHeight.length;i++){
+         let height1 = this.listHeight[i]
+         let height2 = this.listHeight[i+1]
+         if(!height2 || (this.scrollY >= height1 && this.scrollY<height2)){
+           return i
+         }
+       }
+       return 0
+     }
   },
   created(){
     this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
@@ -36,8 +77,8 @@ export default {
         this.goods = res.data.data;
         this.$nextTick(()=>{  //在html渲染完成后再执行此中的代码
           this._initScroll()
+          this._calculateHeight()
         })
-        
       }
     })
   },
@@ -46,6 +87,36 @@ export default {
       this.menuScroll = new BScroll(this.$refs.menuWrapper,{
         click:true
       })
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper,{
+        click:true,
+        probeType:3
+      })
+      this.foodsScroll.on('scroll',pos=>{
+        // console.log(pos)
+        this.scrollY = Math.abs(Math.round(pos.y))
+      })
+    },
+    // 左右联动效果
+    selectMenu(index,event){
+      this.currentIndex = index;
+      // console.log(index,event)
+      if(!event._constructed){
+        return
+      }
+      let foodList = this.$refs.foodList
+      let el = foodList[index]
+      this.foodsScroll.scrollToElement(el,1000)
+    },
+    // 计算右边的高度
+    _calculateHeight(){
+      let foodList = this.$refs.foodList;
+      let height = 0;
+      this.listHeight.push(height)
+      for(let i =0;i<foodList.length;i++){
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
     }
   }
 }
@@ -102,5 +173,56 @@ export default {
           vertical-align middle
           border-1px(rgba(7,17,27,0.1))
           font-size 12px
+  .foods-wrapper
+    flex 1
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147,153,159)
+      background #f3f5f7
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-1px(rgba(7,17,27,0.1))
+      &:last-child
+        border-none()
+        margin-bottom  0px
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+      .content
+        flex 1
+        .name
+          margin 2px 0 8px 0
+          height 14px
+          line-height 14px
+          font-size 14px
+          color rgb(7,17,27)
+        .desc,.extra
+          line-height 10px
+          font-size 10px
+          color rgb(147,153,159)
+        .desc
+          line-height 12px
+          margin-bottom 8px
+        .extra
+          .count
+            margin-right 12px
+        .price
+          font-weight 700
+          line-height 24px
+          .now
+            margin-right 8px
+            font-size 14px
+            color rgb(240,20,20)
+          .old
+            text-decoration line-through
+            font-size 10px
+            color rgb(147,153,159)
+
 </style>
 
